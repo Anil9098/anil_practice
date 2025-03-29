d7db50376b4cd236080ced81ff512a4451b7bde2
Anil <anilkumawat@gmail.com>
2025-03-29 11:40:47 +0530

    commit

--- bash/deploy_ssh.sh
+++ bash/deploy_ssh.sh
@@ -4,10 +4,9 @@ set -e
 
 # ssh on ec2 and run commands
 
-ssh -i ~/.ssh/id_rsa ubuntu@13.233.100.250 << EOF
+ssh -T ~/.ssh/id_rsa ubuntu@13.233.100.250 << EOF
   # Commands to run on the EC2 instance
   echo "Running script on EC2 instance"
-  /home/ubuntu/anil_practice/bash/example_deployment.sh
 EOF
 
 
