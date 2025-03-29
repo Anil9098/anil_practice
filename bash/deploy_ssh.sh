2a51bfc67b1eed49f16679d3285ef68126249a1f
Anil <anilkumawat@gmail.com>
2025-03-29 12:53:49 +0530

    commit

--- bash/deploy_ssh.sh
+++ bash/deploy_ssh.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -e
 
 # ssh on ec2 and run commands
+#ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 << EO  # Commands to run on the EC2 instance
+#  echo "Running script on EC2 instance"
+#EOF
 
-ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 << EOF
-  # Commands to run on the EC2 instance
-  echo "Running script on EC2 instance"
-EOF
 
+echo "anil"
+echo "kumawat"
 
 
 
