ebf1b02f66ab8cbeb80dd9ee1f78266755f615b0
Anil <anilkumawat@gmail.com>
2025-03-31 18:15:30 +0530

    commit

--- bash/deploy_ssh.sh
+++ bash/deploy_ssh.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
+aws --version
 
-# ssh on ec2 and run commands
-sudo ssh -i /root/.ssh/id_rsa ubuntu@13.233.100.250 <<EOF
-# Commands to run inside EC2
-echo "Running script on EC2 instance"
-rm -rf Calculator-Web-Application
-git clone https://github.com/Anil9098/Calculator-Web-Application.git
-cd Calculator-Web-Application || { echo "Failed to enter directory"; exit 1; }
-                
-# Additional commands you want to run on EC2
-docker kill app-container
-docker system prune -a -f
-echo "Building Docker image..."
-docker build -t web_application:latest . || { echo "Docker build failed"; exit 1; }
-                
-# Running Docker container
-docker run -d -p 5000:5000 --name app-container web_application:latest || { echo "Docker run failed"; exit 1; }
+public_ip=$(aws ec2 describe-instances --query "Reservations[].Instances[].PublicIpAddress" --output text)
 
+#public_ip=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=server" --query "Reservations[].Instances[].[PublicIpAddress]" --output text)
+
+for ip in $public_ip; do
+    echo "Processing IP: $ip"
+    ssh -i "/home/ncs/Downloads/jenkinsnodekey.pem" ubuntu@$ip <<EOF
+    rm -rf anil_practice
+    git clone https://github.com/Anil9098/anil_practice.git
+    cd anil_practice/bash
+    ./example_deployment.sh
 EOF
-
-
-
-
+done
 
 
 
