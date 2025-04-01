173a3d1e9385dc59667972b82473cda3b1597054
Anil <anilkumawat@gmail.com>
2025-04-01 15:32:04 +0530

    commit

---
+++ ansible_pipeline.groovy
@@ -0,0 +1,37 @@
+node {
+    // Define environment variables
+    def repoUrl = 'https://github.com/Anil9098/anil_practice.git'
+    //def appDir = '/home/ubuntu/anil_practice'
+    //def scriptDir = '/home/ubuntu/anil_practice/bash'
+    def deployScript = 'example_deployment.sh'
+    def ansiblePlaybook = 'deploy.yml'
+    //def inventoryFile = 'inventory.ini'
+
+    try {
+        stage('Preparation') {
+            echo 'Starting deployment process with Ansible'
+        }
+
+        stage('Run Ansible Playbook') {
+            echo 'Running the Ansible Playbook'
+
+            // Run the Ansible playbook to handle the deployment
+            sh "ansible-playbook ${ansiblePlaybook}"
+        }
+
+        stage('Post-Deployment') {
+            echo 'Deployment process completed.'
+        }
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
+        // Notify or handle any post-process, like sending notifications
+        if (currentBuild.result == 'SUCCESS') {
+            echo 'Deployment was successful!'
+        } else {
+            echo 'Deployment failed!'
+        }
+    }
+}
+
