35f76a94861369de3b64fc288ef57c386f37c53f
Anil <anilkumawat@gmail.com>
2025-04-01 17:45:05 +0530

    commit

--- ansible_pipeline.groovy
+++ ansible_pipeline.groovy
@@ -1,60 +1,61 @@
 node {
-    // Define environment variables
-    def repoUrl = 'https://github.com/Anil9098/anil_practice.git'
-    //def appDir = '/home/ubuntu/anil_practice'
-    //def scriptDir = '/home/ubuntu/anil_practice/bash'
-    def deployScript = 'example_deployment.sh'
-    def ansiblePlaybook = 'deploy.yml'
-    //def inventoryFile = 'inventory.ini'
 
-    try {
+        withEnv(['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8']) {
+        // Define environment variables
+        def repoUrl = 'https://github.com/Anil9098/anil_practice.git'
+        //def appDir = '/home/ubuntu/anil_practice'
+        //def scriptDir = '/home/ubuntu/anil_practice/bash'
+        def deployScript = 'example_deployment.sh'
+        def ansiblePlaybook = 'deploy.yml'
+        //def inventoryFile = 'inventory.ini'
+
+        try {
         
-        stage('Install Ansible') {
-            echo 'Checking and installing Ansible if necessary'
-            sh "git --version"
-            sh "python3 --version"
-            sh "ansible --version"
+            stage('Install Ansible') {
+                echo 'Checking and installing Ansible if necessary'
+                sh "git --version"
+                sh "python3 --version"
+                sh "ansible --version"
 
-            // Set the locale environment variables before running Ansible
-            echo 'Setting locale to en_US.UTF-8'
-            sh """
-                export LC_ALL=en_US.UTF-8
-                export LANG=en_US.UTF-8
-                export LANGUAGE=en_US.UTF-8
-            """
-        }
+                // Set the locale environment variables before running Ansible
+                echo 'Setting locale to en_US.UTF-8'
+                sh """
+                    export LC_ALL=en_US.UTF-8
+                    export LANG=en_US.UTF-8
+                    export LANGUAGE=en_US.UTF-8
+                """
+            }
         
-        stage('Preparation') {
-            echo 'Starting deployment process with Ansible'
-            sh "ansible --version"
-        }
+            stage('Preparation') {
+                echo 'Starting deployment process with Ansible'
+                sh "ansible --version"
+            }
 
-        stage('Run Ansible Playbook') {
-            echo 'Running the Ansible Playbook'
+            stage('Run Ansible Playbook') {
+                echo 'Running the Ansible Playbook'
 
-            // Run the Ansible playbook to handle the deployment
-            sh "ansible-playbook ${deploy.yml}"
-        }
+                // Run the Ansible playbook to handle the deployment
+                sh "ansible-playbook ${deploy.yml}"
+            }
 
-        stage('Post-Deployment') {
-            echo 'Deployment process completed.'
-        }
+            stage('Post-Deployment') {
+                echo 'Deployment process completed.'
+            }
 
-    } catch (Exception e) {
-        currentBuild.result = 'FAILURE'
-        throw e
-    } finally {
-        // Notify or handle any post-process, like sending notifications
-        if (currentBuild.result == 'SUCCESS') {
-            echo 'Deployment was successful!'
-        } else {
-            echo 'Deployment failed!'
+        } catch (Exception e) {
+            currentBuild.result = 'FAILURE'
+            throw e
+        } finally {
+            // Notify or handle any post-process, like sending notifications
+            if (currentBuild.result == 'SUCCESS') {
+                echo 'Deployment was successful!'
+            } else {
+                echo 'Deployment failed!'
+            }
         }
     }
+
 }
-
-
-
 
 
 
