4c9d7189935e9a926a809ea1f3011219fc072b4a
Anil <anilkumawat@gmail.com>
2025-04-02 13:03:50 +0530

    commit

--- ansible_pipeline.groovy
+++ ansible_pipeline.groovy
@@ -1,17 +1,5 @@
 node {
 
-        withEnv(['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8']) {
-        // Define environment variables
-        //def ansibleHome = '/home/ncs/Anil/ansible_project'
-        //def ansiblePlaybook = '/home/ncs/Anil/ansible_project/deploy.yml'
-        //def inventoryFile = '/home/ncs/Anil/ansible_project/inventory.ini'
-        //def repoUrl = 'https://github.com/Anil9098/anil_practice.git'
-        //def appDir = '/home/ubuntu/anil_practice'
-        //def scriptDir = '/home/ubuntu/anil_practice/bash'
-        //def deployScript = 'example_deployment.sh'
-        //def ansiblePlaybook = 'deploy.yml'
-        //def inventoryFile = 'inventory.ini'
-
         try {
         
             stage('Hello') {
@@ -47,10 +35,10 @@ node {
             } else {
                 echo 'Deployment failed!'
             }
-        }
     }
-
 }
+
+
 
 
 
