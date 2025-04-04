787eb0237ff8958608be941473116355274d72a6
Anil <anilkumawat@gmail.com>
2025-04-04 11:02:26 +0530

    commit

--- ansible/ansible_pipeline.groovy
+++ ansible/ansible_pipeline.groovy
@@ -1,48 +1,47 @@
 node {
 
-        try {
+    try {
 
-            stage('git clone') {
-                checkout([$class: 'GitSCM',
-	                branches: [[name: '*/master']],  
-                    userRemoteConfigs: [[
-                        url: "https://github.com/Anil9098/anil_practice.git",
-		                credentialsId: "gitCredentialsId"		
-                    ]]
-                ])
-            }
+        stage('git clone') {
+            checkout([$class: 'GitSCM',
+	            branches: [[name: '*/master']],  
+                userRemoteConfigs: [[
+                    url: "https://github.com/Anil9098/anil_practice.git",
+		            credentialsId: "gitCredentialsId"		
+                ]]
+            ])
+        }
 
-            stage('ansible version') {
-                sh "ansible --version"
-            }
+        stage('ansible version') {
+            sh "ansible --version"
+        }
 
             //stage('verify docker community') {
             //    sh "ansible-galaxy collection install community.docker"
             //}
 
-            stage('execute ansible') {
-                ansiblePlaybook(
-                    credentialsId: 'ansiblekey', 
-                    disableHostKeyChecking: true, 
-                    installation: 'ansible', 
-                    inventory: 'ansible/inventory/aws/aws_ec2.yml', 
-                    playbook: 'ansible/playbook.yml', 
-                    vaultTmpPath: ''
-                )
-            }
+        stage('execute ansible') {
+            ansiblePlaybook(
+                credentialsId: 'ansiblekey', 
+                disableHostKeyChecking: true, 
+                installation: 'ansible', 
+                inventory: 'ansible/inventory/aws/aws_ec2.yml', 
+                playbook: 'ansible/playbook.yml', 
+                vaultTmpPath: ''
+            )
+        }
 
-        } catch (Exception e) {
-            currentBuild.result = 'FAILURE'
-            throw e
-        } finally {
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        throw e
+    } finally {
             // Notify or handle any post-process, like sending notifications
-            if (currentBuild.result == 'SUCCESS') {
-                echo 'Deployment successful!'
-            } else {
-                echo 'Deployment failed!'
-            }
-    
-        }      
+        if (currentBuild.result == 'SUCCESS') {
+            echo 'Deployment successful!'
+        } else {
+            echo 'Deployment failed!'    
+        } 
+    }      
 }
 
 
