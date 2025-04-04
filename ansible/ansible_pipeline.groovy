725066a796d98f3ecd946545ea49c74aeb31accb
Anil <anilkumawat@gmail.com>
2025-04-04 12:16:33 +0530

    commit

--- ansible/ansible_pipeline.groovy
+++ ansible/ansible_pipeline.groovy
@@ -1,41 +1,43 @@
 node {
 
     try {
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws_credentials']]) {
+        
+            stage('git clone') {
+                checkout([$class: 'GitSCM',
+                    branches: [[name: '*/master']],  
+                    userRemoteConfigs: [[
+                        url: "https://github.com/Anil9098/anil_practice.git",
+                        credentialsId: "gitCredentialsId"		
+                    ]]
+                ])
+            }
 
-        stage('git clone') {
-            checkout([$class: 'GitSCM',
-	            branches: [[name: '*/master']],  
-                userRemoteConfigs: [[
-                    url: "https://github.com/Anil9098/anil_practice.git",
-		            credentialsId: "gitCredentialsId"		
-                ]]
-            ])
+            stage('ansible version') {
+                sh "ansible --version"
+            }
+
+            stage('verify aws plugin') {
+                sh "ansible-galaxy collection install amazon.aws"
+            }
+
+            stage('aws CLI verification') {
+                sh "aws --version"
+                sh "aws configure"
+            }
+
+            stage('execute ansible') {
+                ansiblePlaybook(
+                    credentialsId: 'ansiblekey', 
+                    disableHostKeyChecking: true, 
+                    installation: 'ansible', 
+                    inventory: 'ansible/inventory/aws/aws_ec2.yml', 
+                    playbook: 'ansible/playbook.yml', 
+                    vaultTmpPath: ''
+                )
+            }
         }
-
-        stage('ansible version') {
-            sh "ansible --version"
-        }
-
-        stage('verify aws plugin') {
-            sh "ansible-galaxy collection install amazon.aws"
-        }
-
-        stage('aws CLI verification') {
-            sh "aws --version"
-            sh "aws configure"
-        }
-
-        stage('execute ansible') {
-            ansiblePlaybook(
-                credentialsId: 'ansiblekey', 
-                disableHostKeyChecking: true, 
-                installation: 'ansible', 
-                inventory: 'ansible/inventory/aws/aws_ec2.yml', 
-                playbook: 'ansible/playbook.yml', 
-                vaultTmpPath: ''
-            )
-        }
-
+        
     } catch (Exception e) {
         currentBuild.result = 'FAILURE'
         throw e
